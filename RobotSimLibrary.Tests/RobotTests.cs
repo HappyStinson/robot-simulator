aa4422d9d1942e75cd34828388cd5e9c325417c9
Rasmus Nordling <rasmus.nordling@gmail.com>
2023-10-07 13:12:42 +0200

    Test GetReportString passing

--- RobotSimLibrary.Tests/RobotTests.cs
+++ RobotSimLibrary.Tests/RobotTests.cs
@@ -5,11 +5,17 @@ namespace RobotSimLibrary.Tests;
 public class RobotTests
 {
     [Fact]
-    public void Get_SimpleStringShouldBeReported()
+    public void Place_PositionShouldBeUpdated()
+    {
+        
+    }
+
+    [Fact] // change to theory?
+    public void GetReportString_PositionShouldBeReported()
     {
         // Arrange
         Robot robot = new();
-        string expected = "Hello Robot";
+        string expected = "The robot is placed at 0,0 and facing North.";
 
         // Act
         string actual = robot.GetReportString();
